831c34a1bf59bc28b6e6b239d90ed129995ba1b5
Tristan Lee <tristan.lee@moovweb.com>
2022-02-04 10:12:23 -0500

    Update router

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+
+  let url = new URL(request.url);
+
+  for (let i = 1; i < 1001; i++) {
+    const route = `/test/${i}`;
+
+    if (url.pathname == route) {
+      return new Response(`route /test/${i}`, {
+        status: 200,
+        headers: new Headers({ 'Content-Type': 'text/html; charset=utf-8' }),
+      });
+    }
+  }
+  
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
   })
